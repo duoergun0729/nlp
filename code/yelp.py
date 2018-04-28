a6b8c8a5ffb8c78752524aba864dc7b67b3bf56a
maidou <250261953@qq.com>
2018-04-28 18:36:02 +0800

    add tempfile

---
+++ code/yelp.py
@@ -0,0 +1,151 @@
+# -*- coding: UTF-8 -*-
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+
+from sklearn.neural_network import MLPClassifier
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import KFold,StratifiedKFold
+
+from keras import metrics
+
+
+#yelp评论文件路径 已经使用https://github.com/Yelp/dataset-examples处理成CSV格式
+yelp_file="/Volumes/maidou/dataset/yelp/dataset/review.csv"
+
+#yelp_file="/mnt/nlp/dataset/review.csv"
+
+
+#词袋模型的最大特征束
+max_features=5000
+
+
+def load_reviews(filename):
+    #CSV格式表头内容：
+    # funny,user_id,review_id,text,business_id,stars,date,useful,cool
+    text=[]
+    stars=[]
+
+    #https://www.cnblogs.com/datablog/p/6127000.html
+    #sep : str, default ‘,’指定分隔符。如果不指定参数，则会尝试使用逗号分隔。分隔符长于一个字符并且不是‘\s+’,将使用python的语法分析器。
+    # 并且忽略数据中的逗号。正则表达式例子：'\r\t'
+    #header: int or list of ints, default ‘infer’
+    # 指定行数用来作为列名，数据开始行数。如果文件中没有列名，设置为None。设置为0则认为第0行是列名
+    #nrows : int, default None 需要读取的行数（从文件头开始算起）。
+    #skiprows : list-like or integer, default None 需要忽略的行数（从文件开始处算起），或需要跳过的行号列表（从0开始）。
+    #skip_blank_lines : boolean, default True如果为True，则跳过空行；否则记为NaN。
+
+    ###
+    #开发阶段读取前10000行
+    df = pd.read_csv(filename,sep=',',header=0,nrows=10000)
+    #print df.head()
+
+    #按照列名直接获取数据 把 list转换成list对象
+    text=list(df['text'])
+    stars=list(df['stars'])
+
+    #显示各个评分的个数
+    print df.describe()
+
+    return text,stars
+
+#使用sklearnr的mlp
+def do_mlp(text,stars):
+
+    # 切割词袋 删除英文停用词
+    vectorizer = CountVectorizer(ngram_range=(1, 1), max_features=5000,stop_words='english',lowercase=True)
+    #vectorizer = CountVectorizer(ngram_range=(1, 1), max_features=5000, stop_words=None, lowercase=True)
+
+    print "vectorizer 参数:"
+    print vectorizer
+    # 该类会统计每个词语的tf-idf权值
+    transformer = TfidfTransformer()
+    # 使用2-gram和TFIDF处理
+    x = transformer.fit_transform(vectorizer.fit_transform(text))
+    #x = vectorizer.fit_transform(text)
+
+    #使用one-hot编码处理
+    #print stars
+    y = LabelEncoder().fit_transform(stars)
+    y=np_utils.to_categorical(y)
+
+    #print y
+
+    # mlp
+    clf = MLPClassifier(solver='lbfgs',
+                        alpha=1e-5,
+                        hidden_layer_sizes=(10, 5),
+                        random_state=1)
+
+    #使用5折交叉验证
+    scores = cross_val_score(clf, x, y, cv=5, scoring='f1_micro')
+    # print scores
+    print("f1: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    scores = cross_val_score(clf, x, y, cv=5, scoring='accuracy')
+    # print scores
+    print("accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+#使用keras的MLP
+def do_keras_mlp(text,stars):
+    # 切割词袋 删除英文停用词
+    vectorizer = CountVectorizer(ngram_range=(1, 1), max_features=max_features,stop_words='english',lowercase=True)
+    #vectorizer = CountVectorizer(ngram_range=(1, 1), max_features=5000, stop_words=None, lowercase=True)
+
+    print "vectorizer 参数:"
+    print vectorizer
+    # 该类会统计每个词语的tf-idf权值
+    transformer = TfidfTransformer()
+    # 使用2-gram和TFIDF处理
+    x = transformer.fit_transform(vectorizer.fit_transform(text))
+    #x = vectorizer.fit_transform(text)
+
+    #我们可以使用从scikit-learn LabelEncoder类。
+    # 这个类通过 fit() 函数获取整个数据集模型所需的编码,然后使用transform()函数应用编码来创建一个新的输出变量。
+    encoder=LabelEncoder()
+    encoder.fit(stars)
+    encoded_y = encoder.transform(stars)
+
+    #构造神经网络
+    def baseline_model():
+        model = Sequential()
+        model.add(Dense(10, input_dim=max_features, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(5, activation='softmax'))
+        # Compile model
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        return model
+    #在 scikit-learn 中使用 Keras 的模型,我们必须使用 KerasClassifier 进行包装。这个类起到创建并返回我们的神经网络模型的作用。
+    # 它需要传入调用 fit()所需要的参数,比如迭代次数和批处理大小。
+    # 最新接口指定训练的次数为epochs
+    clf = KerasClassifier(build_fn=baseline_model, epochs=20, batch_size=32, verbose=0)
+
+    #使用5折交叉验证
+    scores = cross_val_score(clf, x, encoded_y, cv=5, scoring='f1_micro')
+    # print scores
+    print("f1_micro: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    scores = cross_val_score(clf, x, encoded_y, cv=5, scoring='accuracy')
+    # print scores
+    print("accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+
+if __name__ == '__main__':
+
+    text,stars=load_reviews(yelp_file)
+
+    #do_mlp(text,stars)
+    do_keras_mlp(text,stars)
+    #print stars
